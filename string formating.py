8fbdd5076280899389a69aa740b0a10acb4944fc
bjsadiq <bjsadiq@gmail.com>
2022-10-03 14:39:47 +0100

    first commit

---
+++ string formating.py
@@ -0,0 +1,16 @@
+string1 = "This is a string"
+string2 = "I love this string"
+
+print (string1+ " " +string2)
+print (string1, string2)
+
+print(string1[8])
+
+print(string1[-1])
+
+a1 = "45567"
+
+print(a1[3])
+
+print(string2[1:15:2])
+print(string2[1:7])
